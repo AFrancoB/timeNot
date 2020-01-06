0f440cab8d115c37be2d7ccfab441b015c358329
Alejandro <afrancob17@gmail.com>
2020-01-06 16:26:52 -0500

    clean up functions, add some types

--- Sound/TimeNot/InfinityFuncs.hs
+++ Sound/TimeNot/InfinityFuncs.hs
@@ -5,6 +5,25 @@ import Control.Lens
 
 import Sound.TimeNot.AST
 
+
+infinitizar:: UTCTime -> Double -> [Event] -> [Event] 
+infinitizar evalT canonDur events =
+  let canonDur' = realToFrac (canonDur*1000000000000) :: NominalDiffTime
+  in concat $ zipWith (\canon nextCanonStartTime -> (infTimesToCanon canon evalT nextCanonStartTime ))
+     (repeat events) 
+     (map (canonDur'*) [0..])
+
+infTimesToCanon:: [Event] -> UTCTime -> NominalDiffTime -> [Event]
+infTimesToCanon events evalT nextCanonStartTime = map (\event -> event {time = updateEventTime evalT (time event) nextCanonStartTime}) events
+
+updateEventTime:: UTCTime -> UTCTime -> NominalDiffTime -> UTCTime
+updateEventTime now time nextCanonStartTime = addUTCTime (diffUTCTime (addUTCTime nextCanonStartTime time) now) now 
+
+
+-- test functions
+
+--eventTest = [Event {time = UTCTime 2020-01-02 00:00:00, lengthEvent = 0.5, pitch = 60.0, instrument = "saw", amp = 0.16666666666666666},Event {time = UTCTime 2020-01-02 00:00:00.5, lengthEvent = 0.5, pitch = 62.0, instrument = "saw", amp = 0.16666666666666666}]
+
 today = fromGregorian 2019 07 18
 myUTCTest1 = UTCTime today 30
 
@@ -18,26 +37,3 @@ now = myUTCTest2
 
 toNominalDiffTime n = n :: NominalDiffTime
 canonDuration = toNominalDiffTime 10000000000000
-
---updateEventTime :: (Monad m, Real a) => m UTCTime -> a -> a -> m UTCTime
-updateEventTime:: UTCTime -> UTCTime -> NominalDiffTime -> UTCTime
-updateEventTime now time nextCanonStartTime = addUTCTime (diffUTCTime (addUTCTime nextCanonStartTime time) now) now 
-
-infinitizar:: UTCTime -> Double -> [Event] -> [Event] 
-infinitizar now canonDur canon =
-  let canonDur' = realToFrac (canonDur*1000000000000) :: NominalDiffTime
-  in concat $ zipWith (\canon' nextCanonStartTime -> (func1 canon' now nextCanonStartTime ))
-     (repeat canon) 
-     (map (canonDur'*) [0..])
-
-getTotalDur :: [Event] -> NominalDiffTime
-getTotalDur es = realToFrac (sum $ map lengthEvent es)
-  
-func1 canon now nextCanonStartTime = 
-  map 
-    (\event -> event {time = updateEventTime now (time event) nextCanonStartTime})
-    canon
-
-
-
---eventTest = [Event {time = UTCTime 2020-01-02 00:00:00, lengthEvent = 0.5, pitch = 60.0, instrument = "saw", amp = 0.16666666666666666},Event {time = UTCTime 2020-01-02 00:00:00.5, lengthEvent = 0.5, pitch = 62.0, instrument = "saw", amp = 0.16666666666666666}]
